1d3b183d7fb3a1bdffb77c10406a718e28ca8d04
drnserhio <duran199756@gmail.com>
2021-10-16 00:07:02 +0300

    /

--- hw_1_base_operations/src/main/java/ua/com/alevel/classes/TimeLesson.java
+++ hw_1_base_operations/src/main/java/ua/com/alevel/classes/TimeLesson.java
@@ -10,8 +10,13 @@ public class TimeLesson {
     private static final int VARIABLE_EVEN = 15;
 
     public static String findEndLesson(BufferedReader reader) throws IOException {
+        int i = 0;
+        try {
+            i = Integer.parseInt(reader.readLine());
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Your didn't entry number ");
+        }
 
-        int i = Integer.parseInt(reader.readLine());
         int res = i * LESSON_DURATION + (i / 2) * VARIABLE_UNUVEN + (((i + 1) / 2) - 1) * VARIABLE_EVEN;
 
         return (res / 60 + 9) + " " + (res % 60);
